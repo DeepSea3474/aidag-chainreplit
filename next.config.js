55ac556f5f260db2010b45d75adfce02fbc98afd
DeepSea3474 <redbluealking@hotmail.com>
2025-12-06 16:04:54 +0100

    fix: Removed static export to allow i18n on Netlify deployment

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
-/** @type {import('next/types').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  i18n: {
-    // 8 Dil Desteği
-    locales: ['en', 'tr', 'de', 'fr', 'ar', 'ru', 'es', 'zh'],
-    // Varsayılan Dil
-    defaultLocale: 'en',
-    // Alan Adı Alt Alan Yönlendirmesini Devre Dışı Bırak (Termux için zorunlu)
+  i18n: { // <-- output: 'export' satırı artık yok
+    locales: ['tr', 'en', 'de', 'fr', 'ar', 'ru', 'es', 'zh'],
+    defaultLocale: 'tr',
     localeDetection: false,
   },
-  output: 'export', // Next.js'in statik HTML dosyaları oluşturmasını sağlar (Cloudflare için gerekli)
-  images: {
-    unoptimized: true, // Statik çıktı için resim optimizasyonunu devre dışı bırakır
-  },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
