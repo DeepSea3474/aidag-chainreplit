5c0a88a978179a31a4a903c1025f3b791d03932e
DeepSea3474 <redbluealking@hotmail.com>
2025-12-07 02:57:34 +0100

    Exclude large pack files and modularize imports

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compress: true,
+  experimental: {
+    // büyük bundle'ları bölmek için
+    modularizeImports: {
+      lodash: {
+        transform: "lodash/{{member}}",
+      },
+    },
+  },
 };
 
 module.exports = nextConfig;
+
